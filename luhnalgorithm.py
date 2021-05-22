b42725cf0f62598895aadb10dd9bcbb846150706
Daniel Schjetne <71273660+danielschjetne@users.noreply.github.com>
2021-05-22 04:09:50 +0200

    update
    small corrections

--- luhnalgorithm.py
+++ luhnalgorithm.py
@@ -7,25 +7,25 @@ check_digit = card_number.pop()
 # Reversing the card number
 card_number.reverse()
 
-# Creating an empty list to store our digits
+# Creating an empty list to store the card number
 processed_digits = []
 
-# Defining the for-loop.
+# Main for loop to check each digit of the card number
 for index, digit in enumerate(card_number):
     if index % 2 == 0:  # Even index
         doubled_digit = int(digit) * 2  # Doubling the digit
         if doubled_digit > 9:  # Checking if digit is > 9
-            doubled_digit -= 9  # Subtracting 9
+            doubled_digit -= 9  # Subtracting 9 if > 9
         processed_digits.append(doubled_digit)
 
     else:
         processed_digits.append(int(digit))
 
-# Creating the final number
-total = int(check_digit) + sum(processed_digits)
+# Generating the result
+result = int(check_digit) + sum(processed_digits)
 
-# Checking if the total is divisible by 10
-if total % 10 == 0:
+# Checking if the result is divisible by 10, and therefore, valid.
+if result % 10 == 0:
     print('Valid card number!')
 else:
     print('Invalid card number!')
